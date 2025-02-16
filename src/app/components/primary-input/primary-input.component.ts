ef46577fab6d240e0623bf8bec25cadd5b9a3ce3
Gerfy <gerfynlol@gmail.com>
2025-02-15 23:57:46 -0300

    add: label

--- src/app/components/primary-input/primary-input.component.ts
+++ src/app/components/primary-input/primary-input.component.ts
@@ -13,4 +13,5 @@ export class PrimaryInputComponent {
   @Input() type: InputTypes = "text";
   @Input() formName: string = "";
   @Input() placeholder: string ="";
+  @Input() label: string = "";
 }
