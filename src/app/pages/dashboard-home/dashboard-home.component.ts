ba328d435d5554ab2a25e0f123178ea6fcf9667d
Gerfy1 <gerfynlol@gmail.com>
2025-03-04 00:40:59 -0300

    add calendar functionality to dashboard home with environment configuration

--- src/app/pages/dashboard-home/dashboard-home.component.ts
+++ src/app/pages/dashboard-home/dashboard-home.component.ts
@@ -1,11 +1,68 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { CalendarEvent, CalendarEventTimesChangedEvent, CalendarModule } from 'angular-calendar';
+import { isSameDay } from 'date-fns';
+import { Subject } from 'rxjs';
+
 
 @Component({
   selector: 'app-dashboard-home',
-  imports: [],
+  imports: [ CommonModule, CalendarModule ],
   templateUrl: './dashboard-home.component.html',
   styleUrl: './dashboard-home.component.scss'
 })
-export class DashboardHomeComponent {
+export class DashboardHomeComponent implements OnInit {
 
+  refresh: Subject<any> = new Subject();
+
+  ngOnInit() {
+    this.activeDayIsOpen = false;
+  }
+
+  activeDayIsOpen: boolean = false;
+  events: CalendarEvent[] = [
+    {
+      start: new Date(),
+      title: 'An event',
+    },
+    {
+      start: new Date(),
+      end: new Date(),
+      title: 'Another event',
+    }
+  ];
+  viewDate: Date = new Date();
+
+  dayClicked({ date, events }: { date: Date; events: CalendarEvent[] }): void {
+    if (isSameMonth(date, this.viewDate)) {
+      if (
+        (isSameDay(this.viewDate, date) && this.activeDayIsOpen === true) ||
+        events.length === 0
+      ) {
+        this.activeDayIsOpen = false;
+      } else {
+        this.activeDayIsOpen = true;
+        this.viewDate = date;
+      }
+    }
+  }
+
+  eventTimesChanged({
+    event,
+    newStart,
+    newEnd
+  }: CalendarEventTimesChangedEvent): void {
+    event.start = newStart;
+    event.end = newEnd;
+    this.refresh.next(null);
+  }
+
+  handleEvent(action: string, event: CalendarEvent): void {
+    console.log(action, event);
+  }
 }
+
+function isSameMonth(date: Date, viewDate: Date): boolean {
+  return date.getMonth() === viewDate.getMonth() && date.getFullYear() === viewDate.getFullYear();
+}
+
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,25 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { environment } from './environments/environment';
+import { DateAdapter } from 'angular-calendar';
+import { routes } from './app/app.routes';
+import { CalendarModule } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+if (environment.production){
+  enableProdMode();
+}
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    provideAnimations(),
+    importProvidersFrom(
+      CalendarModule.forRoot({ provide: DateAdapter, useFactory: adapterFactory })
+    )
+  ]
+}).catch((err) => console.error(err));
