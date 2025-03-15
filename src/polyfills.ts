5feb61f7b24dfe0afac385f35978471e7a5be704
Gerfy1 <gerfynlol@gmail.com>
2025-03-15 18:59:13 -0300

    refactor: remove polyfills.ts and update tsconfig.app.json

--- src/polyfills.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Este arquivo inclui polyfills necessários para o aplicativo Angular.
- */
-
-// Zone JS é necessário para o funcionamento do Angular
-import '../node_modules/zone.js/dist/zone';  // Incluído com Angular CLI.
