78f8856f599a414e6a9ddc2594f1a7f582df2e39
Gerfy1 <gerfynlol@gmail.com>
2025-03-15 19:01:36 -0300

    remove import

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-import './polyfills.ts';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
